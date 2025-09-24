c679a99f7f5a60045d7ba94c79f65d2de158afa6
User Guy <decoding.c.t@gmail.com>
2025-09-24 17:05:33 +0300

    create 3 pages on toolbar

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,13 +1,27 @@
+
 import { Home } from './cmps/Home.jsx'
 
+const { useState } = React
+
 export function App() {
+
+    const [page, setPage] = useState('home')
+
+
     return (
         <section className="app">
             <header className="app-header">
                 <h1>My App</h1>
+                <nav>
+                    <span onClick={() => setPage('home')} >Home Page</span>
+                    <span onClick={() => setPage('about')} >About Us</span>
+                    <span onClick={() => setPage('bookindex')} >Book Index</span>
+                </nav>
             </header>
             <main class="container">
-                <Home />
+                {page === 'home' && <Home />}
+                {page === 'about'}
+                {page === 'bookindex'}
             </main>
         </section>
     )
--- cmps/Home.jsx
+++ cmps/Home.jsx
@@ -1,6 +1,9 @@
 import { UserPreview } from './UserPreview.jsx'
 
 export function Home() {
+
+
+
     return (
         <section>
             <h2>Home Sweet Home</h2>
