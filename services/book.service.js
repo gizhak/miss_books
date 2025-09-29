cb6ba5df013b677fc1e0142b057eaccd4cefd311
User Guy <decoding.c.t@gmail.com>
2025-09-29 22:18:34 +0300

    fix images show1

--- services/book.service.js
+++ services/book.service.js
@@ -83,7 +83,7 @@ function _createBooks() {
             description: utilService.makeLorem(20),
             pageCount: utilService.getRandomIntInclusive(20, 600),
             categories: [ctgs[utilService.getRandomIntInclusive(0, ctgs.length - 1)]],
-            thumbnail: `../assets/img/${i + 1}.jpg`,
+            thumbnail: `./assets/img/${i + 1}.jpg`,
             language: "en",
             listPrice: {
                 amount: utilService.getRandomIntInclusive(80, 500),
