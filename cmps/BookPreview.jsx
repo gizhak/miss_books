b0c85a7d420fb5b78d65cd2ed1328b831805d8fe
User Guy <decoding.c.t@gmail.com>
2025-10-02 12:38:00 +0300

    fix some issues

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -2,6 +2,14 @@
 
 export function BookPreview({ book }) {
 
+    function getSaleBook(isOnSale) {
+        const onSale = book.listPrice.isOnSale
+        console.log(onSale)
+        if (onSale) return 'Yes'
+        return 'No'
+
+    }
+
     const { title, thumbnail } = book
     return (
         <article className="book-preview">
@@ -9,6 +17,8 @@ export function BookPreview({ book }) {
             <img src={thumbnail} alt="book Image" />
             <h3>Title: {title}</h3>
             <h4>Price: {book.listPrice.amount}</h4>
+            <h4>On Sale: {getSaleBook(book.listPrice.isOnSale)}</h4>
+
         </article>
     )
 }
